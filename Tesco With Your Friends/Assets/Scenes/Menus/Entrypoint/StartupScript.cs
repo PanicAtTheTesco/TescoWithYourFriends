56e02d3a5bcc03520e520027615c7e53355f46d1
Mathew Dacosta <72082814+mathewdacosta@users.noreply.github.com>
2022-01-17 12:15:01 +0000

    Continue working on main menu

--- Tesco With Your Friends/Assets/Scenes/Menus/Entrypoint/StartupScript.cs
+++ Tesco With Your Friends/Assets/Scenes/Menus/Entrypoint/StartupScript.cs
@@ -3,26 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneLoader : MonoBehaviour
+public class StartupScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
+        // Set up global state
+        // TODO
 
+        // Switch to main menu
+        SceneManager.LoadScene("MainMenu");
     }
 
     // Update is called once per frame
     void Update()
     {
-    }
-
-    public void LoadLevel()
-    {
-        SceneManager.LoadScene("Level_01");
-    }
-
-    public void QuitGame()
-    {
-        Application.Quit();
+        
     }
 }
