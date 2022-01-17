6768f7c42fd63d8108199bf2d3013cf7dca87902
Mathew Dacosta <72082814+mathewdacosta@users.noreply.github.com>
2022-01-17 11:06:54 +0000

    Implement a main menu

---
+++ Tesco With Your Friends/Assets/Scenes/Menus/SceneLoader.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene("Level_01");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
