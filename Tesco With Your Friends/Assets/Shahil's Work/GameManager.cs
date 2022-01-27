2e4169c9aaadd4e2d1d3de6de710584996bee6dc
Shahil Saha <71847625+shahilsaha05uk@users.noreply.github.com>
2022-01-27 10:29:16 +0000

    Revert "Revert "Updates""
    This reverts commit ebfee6dd228d065e38be1444191f13d07705d79a.

---
+++ Tesco With Your Friends/Assets/Shahil's Work/GameManager.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+
+    [Space(5)][Header("References")]
+    public PowerUpScript _powerUpScript;
+    public UI _ui;
+    
+
+    [Space(5)][Header("Prefabs")]
+    public GameObject playerPrefab;
+    public GameObject enemyPrefab;
+
+    [Space(5)] [Header("Prefabs")]
+    public bool pause;
+
+    private void Awake()
+    {
+        Instance = this;
+
+        References();
+        DefaultValues();
+    }
+
+    private void References()
+    {
+        _powerUpScript = GetComponent<PowerUpScript>();
+    }
+    private void DefaultValues()
+    {
+        pause = false;
+    }
+
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.U))
+        {
+            _powerUpScript.SwapPosition();
+        }
+
+
+        if(Input.GetKeyDown(KeyCode.P) && !pause)
+        {
+            _ui.ShowPause();
+        }
+        else if(Input.GetKeyDown(KeyCode.P) && pause)
+        {
+            _ui.HidePause();
+        }
+    }
+
+
+    public Vector3 GetPosition(GameObject obj)
+    {
+        return obj.transform.position;
+    }
+}
