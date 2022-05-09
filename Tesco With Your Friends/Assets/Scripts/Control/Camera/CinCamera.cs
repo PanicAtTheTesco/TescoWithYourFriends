ae69a466fe976c1408a79d99e7edd7dfeb97f4de
Glen Nelson <70724924+glennelson1@users.noreply.github.com>
2022-05-09 14:54:29 +0100

    cinCamera

--- Tesco With Your Friends/Assets/Scripts/Control/Camera/CinCamera.cs
+++ Tesco With Your Friends/Assets/Scripts/Control/Camera/CinCamera.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class CinCamera : MonoBehaviour
 {
-    public float speed;
+
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.right * speed * Time.deltaTime);
+       
     }
 }
--- Tesco With Your Friends/Assets/Scripts/Control/Camera/SwitchCamera.cs
+++ Tesco With Your Friends/Assets/Scripts/Control/Camera/SwitchCamera.cs
@@ -6,14 +6,29 @@ public class SwitchCamera : MonoBehaviour
 {
     public GameObject BallCam;
     public GameObject FreeCam;
-    [SerializeField]
-    private float m_time = 8;
+
+    public GameObject m_ControlUI;
+    public GameObject m_SliderUI;
+    public GameObject m_timeUI;
+    public GameObject m_CinCamUI;
+
+    bool m_inCinCamera;
+
+    private float m_time = 15.5f;
 
     // Start is called before the first frame update
     void Start()
     {
+        m_inCinCamera = true;
+
         BallCam.SetActive(false);
         FreeCam.SetActive(true);
+
+        m_ControlUI.SetActive(false);
+        m_SliderUI.SetActive(false); 
+        m_timeUI.SetActive(false);
+        m_CinCamUI.SetActive(true);
+       
     }
 
     // Update is called once per frame
@@ -21,11 +36,34 @@ public class SwitchCamera : MonoBehaviour
     {
         m_time -= Time.deltaTime;
 
-        if (m_time <= 0.0f)
+        if (m_time <= 0.0f && m_inCinCamera)
         {
             BallCam.SetActive(true);
             FreeCam.SetActive(false);
+
+            m_ControlUI.SetActive(true);
+            m_SliderUI.SetActive(true);
+            m_timeUI.SetActive(true);
+            m_CinCamUI.SetActive(false);
+
+            m_inCinCamera = false;
         }
+
+        if(m_time > 0 && Input.GetKeyDown(KeyCode.F) && m_inCinCamera)
+        {
+            BallCam.SetActive(true);
+            FreeCam.SetActive(false);
+
+            m_ControlUI.SetActive(true);
+            m_SliderUI.SetActive(true);
+            m_timeUI.SetActive(true);
+            m_CinCamUI.SetActive(false);
+
+            m_inCinCamera = false;
+        }
+           
+
+            
 
         if (Input.GetKeyDown(KeyCode.C))
         {
